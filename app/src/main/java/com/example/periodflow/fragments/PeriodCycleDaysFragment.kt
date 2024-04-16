6ecade4ce60c6f534f53325ec2b887cbc64905f0
UsmanKhan15 <usmankhan479301617@gmail.com>
2024-04-16 17:48:15 +0500

    New UI mean New App

--- app/src/main/java/com/example/periodflow/fragments/PeriodCycleDaysFragment.kt
+++ app/src/main/java/com/example/periodflow/fragments/PeriodCycleDaysFragment.kt
@@ -1,4 +1,4 @@
-package com.example.periodflow
+package com.example.periodflow.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.periodflow.R
 import com.example.periodflow.databinding.FragmentPeriodCycleDaysBinding
 
 class PeriodCycleDaysFragment : Fragment() {
